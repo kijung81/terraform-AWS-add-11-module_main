d293cd5b733398495933f0b88c17862bf4b19c4d
MZC01-MYNECK <mzc01-myneck@MZC01-MYNECK.local>
2023-10-19 21:28:47 +0900

    11module-main

--- output.tf
+++ output.tf
@@ -1,19 +1,19 @@
 output "web1_public_dns" {
-  value = module.web.web1_public_dns
+  value = module.add-11-module_web.web1_public_dns
 }
 
 output "web2_public_dns" {
-  value = module.web.web2_public_dns
+  value = module.add-11-module_web.web2_public_dns
 }
 
 output "was1_public_dns" {
-  value = module.was.was1_public_dns
+  value = module.add-11-module_was.was1_public_dns
 }
 
 output "was2_public_dns" {
-  value = module.was.was2_public_dns
+  value = module.add-11-module_was.was2_public_dns
 }
 
 output "rds_cluster_endpoint" {
-  value = module.rds.rds_endpoint
+  value = module.add-11-module_rds.rds_endpoint
 }
